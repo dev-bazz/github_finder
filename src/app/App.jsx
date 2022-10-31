e1494ca1b0051a31ccdb78acdeff28954b148745
bazz <clement.f.bazz@gmail.com>
2022-10-31 20:19:40 +0100

    Update

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,6 +3,7 @@ export default function App() {
         return (
                 <>
                 <h1>Hello World</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi saepe eligendi amet, quod alias sit nostrum ducimus deserunt quo.</p>
                 </>
         )
 }
