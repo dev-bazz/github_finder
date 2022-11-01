2049042ffa8c8a4994b6b48bf712f5cd39303c54
bazz <clement.f.bazz@gmail.com>
2022-11-01 21:57:16 +0100

    update

---
+++ src/app/pages/About.jsx
@@ -0,0 +1,11 @@
+
+
+export default function About() {
+        return (
+                <div>
+
+                        <h1>About Developer</h1>
+                        <h2>Clement Femi Bazuaye</h2>
+                </div>
+        )
+}
