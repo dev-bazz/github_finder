60e46564194a7893d8c24b8cfdd4ce51aabfb939
bazz <clement.f.bazz@gmail.com>
2022-11-01 08:42:00 +0100

    Setting File Structure

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,11 @@
+import Navbar from "./components/layouts/Navbar";
+
 
 export default function App() {
         return (
                 <>
-                <h1>Hello World</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi saepe eligendi amet, quod alias sit nostrum ducimus deserunt quo.</p>
+                <Navbar />
+                
                 </>
         )
 }
---
+++ src/app/components/layouts/Navbar.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Navbar() {
+        return (
+                <nav className="navbar">
+                        <div className="container">hello</div>
+                </nav>
+        )
+}
